187440a176e20d0c8a64feb68f854b3d828f2c33
Mark Dyhr <1304842+controld@users.noreply.github.com>
2018-08-01 16:55:53 -0500

    rewrote async functions to fetch data through NOAA apis

--- src/wthrFetch.js
+++ src/wthrFetch.js
@@ -1,74 +1,54 @@
-const fetchPointData = async () => {
-  // experiment with various NOAA weather APIs to see what they return
-  const doSomething = async (lat, lon) => {
-    console.log(`doSomething: lat=${lat} lon=${lon}`)
-    let localPoints = 'https://api.weather.gov/points/'
-    let gridPoints = 'https://api.weather.gov/gridpoints/'
-    let forecast = 'https://api.weather.gov/points/'
-    let wthrResults = new Map()
-
-    const pointsUrl = `${localPoints}${lat},${lon}`
-    let json
-    try {
-      const res = await window.fetch(pointsUrl)
-      json = await res.json()
-      wthrResults.set('localPoints', json)
-      console.log(`cwa=${json.properties.cwa} gridX=${json.properties.gridX} gridY=${json.properties.gridY}`)
-    } catch (err) {
-      console.log(`fetch or JSON parse for ${pointsUrl} failed, err=%O`, err)
-      return wthrResults
-    }
-    const gridPointsUrl = `${gridPoints}${json.properties.cwa}/${json.properties.gridX},${json.properties.gridY}`
-    console.log(`gridPointsUrl=${gridPointsUrl}`)
-    try {
-      const gridPointsRes = await window.fetch(gridPointsUrl)
-      const gridPointsJson = await gridPointsRes.json()
-      wthrResults.set('gridPoints', gridPointsJson)
-      console.log(`gridPointsJson=%O`, gridPointsJson)
-    } catch (err) {
-      console.log(`fetch or JSON parse for ${gridPointsUrl} failed, err=%O`, err)
-      return wthrResults
-    }
-    const forecastUrl = `${forecast}${lat},${lon}`
-    console.log(`forecastUrl=${forecastUrl}`)
-    let forecastJson
-    try {
-      const pointsForecast = await window.fetch(forecastUrl)
-      forecastJson = await pointsForecast.json()
-      wthrResults.set('pointsForecast', forecastJson)
-      console.log(`forecastJson=%O`, forecastJson)
-    } catch (err) {
-      console.log(`fetch or JSON parse for ${forecastUrl} failed, err=%O`, err)
-      return wthrResults
-    }
-    const stationForecastUrl = forecastJson.properties.forecast
-    console.log(`stationForecastUrl=${stationForecastUrl}`)
-    try {
-      const stationForecast = await window.fetch(stationForecastUrl)
-      const stationForecastJson = await stationForecast.json()
-      wthrResults.set('stationForecast', stationForecastJson)
-      console.log(`stationForecastJson=%O`, stationForecastJson)
-    } catch (err) {
-      console.log(`fetch or JSON parse for ${stationForecastUrl} failed, err=%O`, err)
-      return wthrResults
-    }
-    let stationsUrl = `${forecast}${lat},${lon}/stations`
-    let stationListJson
-    try {
-      const stationList = await window.fetch(stationsUrl)
-      stationListJson = await stationList.json()
-      wthrResults.set('stationList', stationListJson)
-      console.log(`stationListJson=%O`, stationListJson)
-    } catch (err) {
-      console.log(`fetch or JSON parse for ${stationsUrl} failed, err=%O`, err)
-    }
-    console.log(`wthrResults=%O`, wthrResults)
-  }
-
-  // kick off weather info fetch by first getting latitude and longitude
-  navigator && navigator.geolocation.getCurrentPosition((position) => {
-    doSomething(position.coords.latitude, position.coords.longitude)
-  })
+// See: https://forecast-v3.weather.gov/documentation?redirect=legacy
+//
+// TODO:
+//  put URLs in constants
+//  convert from fetch to Axios, because it's all the rage
+//  test with https to see if Safari works
+//  explore other APIs
+//
+const fetchPointData = async (lat, lon) => {
+  // From https://forecast-v3.weather.gov/documentation?redirect=legacy:
+  // Metadata about a point. This is the primary endpoint for forecast information for a location.
+  // It contains linked data for the forecast, the hourly forecast, observation and other information.
+  const localPoints = 'https://api.weather.gov/points/'
+  const pointsUrl = `${localPoints}${lat},${lon}`
+  return window.fetch(pointsUrl)
+}
+// wfo is the weather forecast office id
+// See https://en.wikipedia.org/wiki/List_of_National_Weather_Service_Weather_Forecast_Offices
+const fetchGridPointsData = async (wfo, gridX, gridY) => {
+  // Raw (commonly referred to as "gridded") data provided by the Weather Office.
+  // Every forecast request will use this data to build the forecast response.
+  // The grid for a given location is determined by the "property.forecastGridData"
+  // property in the /points/{lat},{lon} endpoint.
+  const gridPoints = 'https://api.weather.gov/gridpoints/'
+  const gridPointsUrl = `${gridPoints}${wfo}/${gridX},${gridY}`
+  return window.fetch(gridPointsUrl)
+}
+// /points/{point}/forecast
+// For instance: "https://api.weather.gov/gridpoints/MPX/109,69/forecast"
+const fetchStationForecast = async (stationForecastUrl = 'https://api.weather.gov/gridpoints/MPX/109,69/forecast') => {
+  return window.fetch(stationForecastUrl)
+}
+// /points/{point}/stations
+// For instance: "https://api.weather.gov/gridpoints/MPX/109,69/stations"
+// Stations nearest to a point in order of distance.
+const fetchStationList = async (stationsUrl = 'https://api.weather.gov/gridpoints/MPX/109,69/stations') => {
+  return window.fetch(stationsUrl)
+}
+const getLatLon = async () => {
+  return new Promise(
+    (resolve, reject) => {
+      function positionOK (position) {
+        resolve(position)
+      }
+      function err (error) {
+        // failure is always an option with Safari w/o a secure web server
+        console.log(`failed to geolocate: ERROR(${error.code}): ${error.message}`)
+        reject(error)
+      }
+      window.navigator && window.navigator.geolocation.getCurrentPosition(positionOK, err)
+    })
 }
 
-module.exports = fetchPointData
+module.exports = { getLatLon, fetchPointData, fetchGridPointsData, fetchStationForecast, fetchStationList }
